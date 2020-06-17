dafa1c78778837bd6eddd6e1062e9d3e284b0fd6
Shaurya026 <43774789+Shaurya026@users.noreply.github.com>
2020-06-17 20:54:06 +0530

    #650 Div. 3

--- CodeForces/Even_array.py
+++ CodeForces/Even_array.py
@@ -4,13 +4,16 @@ while t > 0 :
     terms = int(input())
     lst = list(map(int,input().split()))
     lst1 = list()
-    answer = 0
+    answer1 = 0
+    answer2 = 0
     for i,j in enumerate(lst):
         lst1.append(i % 2)
         lst[i] = lst[i] % 2
-        if (lst1[i]==1 and lst[i]==0) or (lst1[i]==0 and lst[i]==1):
-            answer += 1  
-    if answer % 2 == 0:
-        print(answer//2)
+        if (lst1[i]==1 and lst[i]==0) :
+            answer1 += 1  
+        elif (lst1[i]==0 and lst[i]==1):
+            answer2 += 1
+    if answer1 == answer2:
+        print(answer1)
     else:
         print(-1)
